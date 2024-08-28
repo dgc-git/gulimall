d4f5d918ee93cd84a4f031986bea79da1c234416
dgc-git <18280349792@163.com>
2024-08-29 00:06:45 +0800

    ...

--- gulimall-cart/src/main/java/com/atguigu/gulimall/cart/controller/CartController.java
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/controller/CartController.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.cart.controller;
 import com.atguigu.common.constant.AuthServerConstant;
 import com.atguigu.gulimall.cart.interceptor.CartInterceptor;
 import com.atguigu.gulimall.cart.service.CartService;
+import com.atguigu.gulimall.cart.vo.Cart;
 import com.atguigu.gulimall.cart.vo.CartItem;
 import com.atguigu.gulimall.cart.vo.UserInfoTo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.concurrent.ExecutionException;
@@ -18,10 +20,25 @@ import java.util.concurrent.ExecutionException;
 public class CartController {
     @Autowired
     CartService cartService;
+    @GetMapping("/deleteItem")
+    public String deleteItem(@RequestParam("skuId") Long skuId){
+        cartService.deleteItem(skuId);
+        return "redirect:http://cart.gulimall.com/addToCartSuccess.html";
+    }
+    @GetMapping("/countItem")
+    public String countItem(@RequestParam("skuId") Long skuId,@RequestParam("num") Integer num){
+        cartService.changeItemCount(skuId,num);
+        return "redirect:http://cart.gulimall.com/addToCartSuccess.html";
+    }
+    @GetMapping("/checkItem")
+    public String checkItem(@RequestParam("skuId") Long skuId,@RequestParam("check") Integer check){
+        cartService.checkItem(skuId,check);
+        return "redirect:http://cart.gulimall.com/addToCartSuccess.html";
+    }
     @GetMapping("/cart.html")
-    public String cartListPage(){
-        UserInfoTo userInfoTo = CartInterceptor.threadLocal.get();
-
+    public String cartListPage(Model model) throws ExecutionException, InterruptedException {
+        Cart cart=cartService.getCart();
+        model.addAttribute("cart",cart);
         return "cartList";
     }
 
@@ -30,8 +47,15 @@ public class CartController {
      * @return
      */
     @GetMapping("/addToCart")
-    public String addToCart(@RequestParam("skuId") Long skuId, @RequestParam("num") Integer num, Model model) throws ExecutionException, InterruptedException {
-        CartItem cartItem=cartService.addToCart(skuId,num);
+    public String addToCart(@RequestParam("skuId") Long skuId, @RequestParam("num") Integer num, RedirectAttributes ra) throws ExecutionException, InterruptedException {
+        cartService.addToCart(skuId,num);
+//        model.addAttribute("skuId",skuId);
+        ra.addAttribute("skuId",skuId);
+        return "redirect:http://cart.gulimall.com/addToCartSuccess.html";
+    }
+    @GetMapping("/addToCartSuccess.html")
+    public String addToCartSuccessPage(@RequestParam("skuId") Long skuId,Model model){
+        CartItem cartItem=cartService.getCartItem(skuId);
         model.addAttribute("item",cartItem);
         return "success";
     }
--- gulimall-cart/src/main/java/com/atguigu/gulimall/cart/service/impl/CartServiceImpl.java
+++ gulimall-cart/src/main/java/com/atguigu/gulimall/cart/service/impl/CartServiceImpl.java
@@ -6,10 +6,12 @@ import com.atguigu.common.utils.R;
 import com.atguigu.gulimall.cart.feign.ProductFeignService;
 import com.atguigu.gulimall.cart.interceptor.CartInterceptor;
 import com.atguigu.gulimall.cart.service.CartService;
+import com.atguigu.gulimall.cart.vo.Cart;
 import com.atguigu.gulimall.cart.vo.CartItem;
 import com.atguigu.gulimall.cart.vo.SkuInfoVo;
 import com.atguigu.gulimall.cart.vo.UserInfoTo;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -19,6 +21,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -32,30 +35,121 @@ public class CartServiceImpl implements CartService {
     ThreadPoolExecutor executor;
 
     @Override
+    public Cart getCart() throws ExecutionException, InterruptedException {
+        UserInfoTo userInfoTo = CartInterceptor.threadLocal.get();
+        Cart cart = new Cart();
+        if(userInfoTo.getUserId()!=null){
+            //登录
+            String cartKey=CART_PREFIX+userInfoTo.getUserId();
+            String tempCartKey=CART_PREFIX + userInfoTo.getUserKey();
+            BoundHashOperations<String, Object, Object> hashOps = redisTemplate.boundHashOps(cartKey);
+             //如果有临时购物车数据
+            List<CartItem> tempCartItems = getCartItems(tempCartKey);
+            if(tempCartItems!=null && tempCartItems.size()>0) {
+                //有数据
+                for (CartItem item : tempCartItems) {
+                    addToCart(item.getSkuId(), item.getCount());
+                }
+                //清除临时购物车数据
+                clearCart(tempCartKey);
+            }
+            //获取登录后的购物车
+            List<CartItem> cartItems = getCartItems(cartKey);
+            cart.setItems(cartItems);
+        }else {
+            String cartKey = CART_PREFIX+userInfoTo.getUserKey();
+            List<CartItem> cartItems = getCartItems(cartKey);
+            cart.setItems(cartItems);
+            }
+
+
+        return cart;
+    }
+
+    @Override
+    public void deleteItem(Long skuId) {
+        BoundHashOperations<String, Object, Object> cartOps = getCartOps();
+        cartOps.delete(skuId.toString());
+    }
+
+    @Override
+    public void changeItemCount(Long skuId, Integer num) {
+        CartItem cartItem = getCartItem(skuId);
+        cartItem.setCount(num);
+        BoundHashOperations<String, Object, Object> cartOps = getCartOps();
+        cartOps.put(skuId.toString(),JSON.toJSONString(cartItem));
+    }
+
+    @Override
+    public void checkItem(Long skuId, Integer check) {
+        BoundHashOperations<String, Object, Object> cartOps = getCartOps();
+        CartItem cartItem = getCartItem(skuId);
+        cartItem.setCheck(check==1?true:false);
+        String s = JSON.toJSONString(cartItem);
+        cartOps.put(skuId.toString(),s);
+    }
+
+    @Override
+    public void clearCart(String cartKey){
+        redisTemplate.delete(cartKey);
+    }
+    private List<CartItem> getCartItems(String cartKey){
+        BoundHashOperations<String, Object, Object> hashOps = redisTemplate.boundHashOps(cartKey);
+        List<Object> values = hashOps.values();
+        if(values!=null && values.size()>0){
+            List<CartItem> collect = values.stream().map(obj -> {
+                String str = (String) obj;
+                CartItem cartItem = JSON.parseObject(str, CartItem.class);
+                return cartItem;
+            }).collect(Collectors.toList());
+            return collect;
+        }
+        return null;
+    }
+
+    @Override
+    public CartItem getCartItem(Long skuId) {
+        BoundHashOperations<String, Object, Object> cartOps = getCartOps();
+        String o = (String) cartOps.get(skuId.toString());
+        CartItem cartItem = JSON.parseObject(o, CartItem.class);
+        return cartItem;
+    }
+
+    @Override
     public CartItem addToCart(Long skuId, Integer num) throws ExecutionException, InterruptedException {
         BoundHashOperations<String, Object, Object> cartOps = getCartOps();
-        //远程查询商品信息
-        CartItem cartItem = new CartItem();
-        CompletableFuture<Void> getSkuInfo = CompletableFuture.runAsync(() -> {
-            R skuInfo = productFeignService.info(skuId);
-            SkuInfoVo data = skuInfo.getData("skuInfo", new TypeReference<SkuInfoVo>() {
-            });
-            //添加到购物车
-            cartItem.setCheck(true);
-            cartItem.setCount(num);
-            cartItem.setImage(data.getSkuDefaultImg());
-            cartItem.setTitle(data.getSkuTitle());
-            cartItem.setSkuId(skuId);
-            cartItem.setPrice(data.getPrice());
-        }, executor);
-        CompletableFuture<Void> getSkuSalerAttrValues = CompletableFuture.runAsync(() -> {
-            List<String> values = productFeignService.getSkuSaleAttrValues(skuId);
-            cartItem.setSkuAttr(values);
-        }, executor);
-        CompletableFuture.allOf(getSkuInfo,getSkuSalerAttrValues).get();
-        String s = JSON.toJSONString(cartItem);
-        cartOps.put(skuId.toString(), s);
-        return cartItem;
+        String res = (String) cartOps.get(skuId.toString());
+        if (StringUtils.isEmpty(res)) {
+            CartItem cartItem = new CartItem();
+            //远程查询商品信息
+            CompletableFuture<Void> getSkuInfo = CompletableFuture.runAsync(() -> {
+                R skuInfo = productFeignService.info(skuId);
+                SkuInfoVo data = skuInfo.getData("skuInfo", new TypeReference<SkuInfoVo>() {
+                });
+                //添加到购物车
+                cartItem.setCheck(true);
+                cartItem.setCount(num);
+                cartItem.setImage(data.getSkuDefaultImg());
+                cartItem.setTitle(data.getSkuTitle());
+                cartItem.setSkuId(skuId);
+                cartItem.setPrice(data.getPrice());
+            }, executor);
+            CompletableFuture<Void> getSkuSalerAttrValues = CompletableFuture.runAsync(() -> {
+                List<String> values = productFeignService.getSkuSaleAttrValues(skuId);
+                cartItem.setSkuAttr(values);
+            }, executor);
+            CompletableFuture.allOf(getSkuInfo, getSkuSalerAttrValues).get();
+            String s = JSON.toJSONString(cartItem);
+            cartOps.put(skuId.toString(), s);
+            return cartItem;
+        } else {
+            //购物车有此商品
+            CartItem cartItem = JSON.parseObject(res, CartItem.class);
+            cartItem.setCount(cartItem.getCount() + num);
+            cartOps.put(skuId.toString(),JSON.toJSONString(cartItem));
+            return cartItem;
+        }
+
     }
 
     /**
